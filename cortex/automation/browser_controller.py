8866dc3d1ba2e2d34db8bc8d079f9d0218777973
Kunal Kumar <kunalkumar.aiml@gmail.com>
2026-03-15 21:56:22 +0530

    Add browser controller for automation

---
+++ cortex/automation/browser_controller.py
@@ -0,0 +1,10 @@
+class BrowserController:
+    """
+    Controls browser based actions.
+    """
+
+    def open_browser(self):
+        print("Opening browser...")
+
+    def search(self, query):
+        print(f"Searching for: {query}")
