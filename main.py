75e72a75a09825ddf6b4357d32346cd2516c28cf
Kunal Kumar <kunalkumar.aiml@gmail.com>
2026-03-17 19:26:56 +0530

    Connect browser automation tool with Cortex-Agent execution
    Added BrowserTool to perform Google searches based on user tasks.

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from cortex.brain.task_planner import TaskPlanner
+from cortex.tools.browser_tool import BrowserTool
 
 
 def main():
 
     planner = TaskPlanner()
+    browser = BrowserTool()
 
     task = input("Enter your task: ")
 
+    print("\nGenerating AI Plan...\n")
+
     plan = planner.create_plan(task)
 
-    print("\nAI Generated Plan:\n")
+    print("AI Generated Plan:\n")
     print(plan)
+
+    print("\nRunning browser search...\n")
+
+    results = browser.search_google(task)
+
+    print("Top Search Results:\n")
+
+    for r in results:
+        print("-", r)
 
 
 if __name__ == "__main__":
